656a8330940dd7f4b3d68f2b56a55e36de3385d3
Lynx Pardelle <74871143+LynxPardelle@users.noreply.github.com>
2022-04-19 01:05:32 -0500

    Finished

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import words from "./dictionary";
 import { getChars, sortArray } from "./utils";
-
-/* Escanear Dictionary.ts & get */
+/* Scan Dictionary.ts & get All Chars Scored */
 var MyChars: any = {};
 for (let word of words) {
   let chars = getChars(word);
@@ -13,65 +12,76 @@ for (let word of words) {
     }
   }
 }
-
-// MyChars = sortArray();
-
+/* 
 var MyNewChars = [];
-
 for (let char in MyChars) {
-  //  MyNewChars.push(MyChars[char] + "/" + char);
   MyNewChars.push([char, MyChars[char]]);
 }
-
 // MyChars = MyNewChars.sort((a, b) => b[1] - a[1]);
 MyChars = sortArray(MyNewChars, "desc", (c) => c[1]);
-
 var MyObjectChars: any = {};
 for (let char of MyChars) {
   MyObjectChars[char[0]] = char[1];
 }
-
-MyChars = MyObjectChars;
-
-/* Puntuar palabras */
-var MyWords: any = {};
-
+MyChars = MyObjectChars; 
+*/
+/* Score words */
+// var MyWords: any = {};
+var MyWords = [];
 for (let word of words) {
   let chars = getChars(word);
-  let punt = 0;
+  let score = 0;
   for (let char of chars) {
-    punt = punt + MyChars[char];
+    score = score + MyChars[char];
   }
-  MyWords[word] = punt;
+  MyWords.push([word, score]);
 }
-
+/* 
 var MyNewWords = [];
-
 for (let word in MyWords) {
   MyNewWords.push([word, MyWords[word]]);
-}
-
-MyWords = sortArray(MyNewWords, "desc", (c) => c[1]);
-
-/* var MyObjectWords: any = {};
+} 
+*/
+MyWords = sortArray(MyWords, "desc", (c) => c[1]);
+/* 
+var MyObjectWords: any = {};
 for (let word of MyWords) {
   MyObjectWords[word[0]] = word[1];
 }
-
-MyWords = MyObjectWords; */
-
-for (let word of MyWords) {
-  let chars = getChars(word);
+MyWords = MyObjectWords;
+*/
+/* Get A ChoosenWord */
+var notchosenWords = [];
+var chosenWord = "";
+var moreChosenWords = [];
+var i = 0;
+do {
+  if (!MyWords[i] || !MyWords[i][0]) {
+    chosenWord = "ERRORR";
+  }
+  let chars = getChars(MyWords[i][0]);
   let myChars: any = {};
+  let doesntHaveIt = true;
   for (let char of chars) {
     if (myChars[char]) {
       myChars[char]++;
-      MyWords.splice(MyWords.indexOf(word), 1);
+      doesntHaveIt = false;
+      notchosenWords.push(MyWords[i]);
       break;
     } else {
       myChars[char] = 1;
     }
   }
-}
-
+  if (doesntHaveIt === true) {
+    if (chosenWord !== "") {
+      moreChosenWords.push(MyWords[i][0]);
+    }
+    chosenWord = MyWords[i][0];
+  }
+  i++;
+} while (chosenWord === "" || moreChosenWords.length < 5);
+console.log(MyChars);
 console.log(MyWords);
+console.log(notchosenWords);
+console.log(chosenWord);
+console.log(moreChosenWords);
